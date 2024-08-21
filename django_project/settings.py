07620560a012ccd1e2430a20cf1a37fded785916
Jeremias <jeremontoya155@gmail.com>
2024-08-21 08:45:27 -0300

    Subida cambiando el ejs con optimizaciones de vistas cambios rest api un archivo con requiremnets bien hechoa

--- django_project/settings.py
+++ django_project/settings.py
@@ -114,7 +114,7 @@ STATICFILES_DIRS = [
 ]
 
 # STATIC_ROOT should be a separate directory where all static files will be collected for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
